034d00b5452054d6e1b88924ac3453c43f99c1d5
matic.chiu <matic7056@gmail.com>
2016-05-26 23:15:59 +0800

    Add GPI function Add DHT11 function Can show message via COM port

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,25 @@
+#include <DHT.h>
+#include <SoftwareSerial.h>
+
+#define DHT_PIN		2
+#define DHT_TYPE	DHT11
+
+
+SoftwareSerial Serial_1(10, 11);
+const int pinButton = 52;
+DHT dht(DHT_PIN, DHT_TYPE);
+
 void setup() {
 	// put your setup code here, to run once:
 	Serial.begin(57600);
+	Serial_1.begin(57600);
+	
+	pinMode(pinButton, INPUT);
+	
+	dht.begin();
+	
 	while(!Serial);
+	while(!Serial_1);
 }
 
 #define UARTDATA_MAX 100
@@ -10,6 +28,8 @@ int iDataLen = 0;
 byte abyUartData[UARTDATA_MAX];
 unsigned int uiUartDataIndex_Start = 0;
 unsigned int uiUartDataIndex_End = 0;
+
+int iButtonStatus = 0;
 
 unsigned int UartRcvData()
 {
@@ -31,6 +51,21 @@ unsigned int UartRcvData()
 void loop() 
 {
 	UartRcvData();
-	
+	iButtonStatus = digitalRead(pinButton);
+	if(iButtonStatus)
+	{
+		Serial.println("Detect");
+		
+	}
+
+	float h = dht.readHumidity();
+	float t = dht.readTemperature();
+
+	Serial.print("Humidity: ");
+	Serial.print(h);
+	Serial.print(" %\t");
+	Serial.print("Temperature: ");
+	Serial.print(t);
+	Serial.print(" *C \n");	
 }
 
